03770d12eb4bc1f22611613b84a390dcac601e11
EUNJI LEE <xjfmwld@gmail.com>
2021-09-29 22:11:25 +0900

    git start

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+const ENDPOINT = "http://localhost:5000/data";
+
+interface dataProps {
+  id: number;
+  name: string;
+  age: number;
+  job: string;
+}
 
 function App() {
+  const [data, setData] = useState<dataProps>({
+    id: 0,
+    name: "",
+    age: 0,
+    job: "",
+  });
+
+  const getData = async (id: string) => {
+    try {
+      const res = await fetch(ENDPOINT + "/" + id);
+      const json = await res.json();
+      setData(json);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        <li>id : {data.id}</li>
+        <li>name : {data.name}</li>
+        <li>age : {data.age}</li>
+        <li>job : {data.job}</li>
+      </ul>
+      <button onClick={() => getData("1")}>Data 1</button>
+      <button onClick={() => getData("2")}>Data 2</button>
     </div>
   );
 }
